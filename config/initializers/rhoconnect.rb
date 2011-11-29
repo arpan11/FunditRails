d21f304b5829b2aec6aeb04652363f984571be36
arpan11 <arpan@circarconsulting.com>
2011-11-29 15:33:29 +0530

    rhoconnect

--- config/initializers/rhoconnect.rb
+++ config/initializers/rhoconnect.rb
@@ -1,6 +1,6 @@
 Rhoconnect.configure do |config|
-  config.uri = ""
-  config.app_endpoint = ""
+  config.uri = "http://06fee782d49b454ca7a58e6edb82c086@rhoservice0eaf423f.rhoconnect.com"
+  config.app_endpoint = "http://funditrails.heroku.com/"
   config.authenticate = lambda { |credential|    
     return true
   }
