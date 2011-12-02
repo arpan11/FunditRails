bbed024b100515f6bb465c0969a6804914c64ca2
arpan11 <arpan@circarconsulting.com>
2011-12-02 21:29:33 +0530

    asd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,23 @@
 require 'rhoconnect-rb'
+#require 'json'
+#require 'rest_client'
+#require 'rhoconnect/version'
+#require 'rhoconnect/configuration'
+#require 'rhoconnect/client'
+#require 'rhoconnect/resource'
+#require 'rhoconnect/endpoints'
+#require 'rhoconnect/railtie' if defined?(Rails)
 
 class User < ActiveRecord::Base
   include Rhoconnect::Resource
 
   def partition
-  :app
+  lambda { self.user.username }
   end
 
   def self.rhoconnect_query(partition)
-    User.all
+    User.where(:username => partition).all
   end
+
+  
 end
--- config/initializers/rhoconnect.rb
+++ config/initializers/rhoconnect.rb
@@ -1,5 +1,6 @@
 Rhoconnect.configure do |config|
   config.uri = "http://06fee782d49b454ca7a58e6edb82c086@rhoservice0eaf423f.rhoconnect.com"
+  config.token = "06fee782d49b454ca7a58e6edb82c086"
   config.app_endpoint = "http://funditrails.heroku.com/"
   config.authenticate = lambda { |credential|    
     return true
